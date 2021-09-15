8f89ad5e7179cb71736113f8a6268eb7769f1044
Aiden gillespie <aiden@cold.org>
2021-09-15 15:56:25 -0600

    Updated functions for calculator

--- arithmetic.py
+++ arithmetic.py
@@ -41,3 +41,12 @@ def mod(num1, num2):
     """Return the remainder of num1 / num2."""
 
     return num1 % num2
+
+def add_mult(num1,num2,num3):
+
+    return multiply(add(num1, num2), num3)
+
+def add_cube(num1,num2):
+
+    return add(cube(num1), cube(num2))
+    
